f39a836176ef084e8ac569d11b72a8ea83e56555
roddy <jiting.xjt@alibaba-inc.com>
2020-06-21 13:42:15 +0800

    refactor: use lerna to manage repo

--- packages/review/models/user.js
+++ packages/review/models/user.js
@@ -4,5 +4,5 @@ var Schema = mongoose.Schema;
 module.exports = mongoose.model('User', new Schema({
     name: String,
     password: String,
-    admin: Boolean 
+    admin: Boolean
 }));
--- public/server.js
+++
@@ -1,6 +0,0 @@
-var product = require('./api/product-service');
-var PORT = process.env.PORT || 3029;
-
-var server_app = product.listen(PORT, function() {
-  console.log('Express server for product service listening on port ' + PORT);
-});
